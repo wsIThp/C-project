066170aa297acda1dc06a12f772bae370479b19c
xiaoxin:wq <593748946@qq.com>
2011-08-03 11:42:51 +0800

    插入   删除

--- danlian2.c
+++ danlian2.c
@@ -48,6 +48,10 @@ STU *add_note(STU *p)
     STU *head=p;
     STU *p_c=NULL;
     p_c=malloc(sizeof(STU));
+    if(p==NULL)
+    {
+        
+    }
     if(p_c==NULL)
     {
         perror("create new");
@@ -73,9 +77,62 @@ STU *add_note(STU *p)
 
     return head;
 }
+/*STU *remove_link(STU *q)
+{
+    int i;
+    //STU *P=NULL;
+    STU *ptr=NULL;
+    scanf("%d",&i);
+    q->next=ptr;
+    while(ptr->number==q->next->number==i)    
+    {
+        q->next=ptr->next;
+        free(ptr);
+        return head; 
+    }
+}*/
+STU *remove_link(STU *q)
+{
+    int i=0;
+    STU *temp=NULL;
+    STU *h=q;
+    if(q==NULL)
+    {
+        printf("empty");
+        return NULL;
+    }
+    scanf("%d",&i);
 
+    if (q->number==i)
+    {
+        h=q->next;
+        free(q);
+        return h;
+    }
+    while(q->next!=NULL)
+    {
+        if(q->next->number==i)
+        {
+            break;
+        }
+        q=q->next;
+     }
+     if (q->next==NULL)
+     {
+         printf("no number\n");
+         return h;
+     }
+    temp=q->next;  
+    q->next=q->next->next;
+    free(temp);
+    return h;
+}
 void print_link(STU *p)
 {
+    if (p==NULL)
+    {
+        printf("empty");
+    }
 	while(p != NULL)
 	{
 		printf("%-5d  %s\n", p->number,p->name);
@@ -88,7 +145,8 @@ int main(void)
 	STU *head = NULL;
     
 	head = create_link(10);
-    add_note(head);
+   //head=add_note(head);
+    head =  remove_link(head);
 	print_link(head);
 	return 0;
 }
