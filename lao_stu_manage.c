c367fecb5a26dc8a3381eb38d7f5a258e2d380c4
xiaoxin:wq <593748946@qq.com>
2011-08-14 17:25:25 +0800

    学生管理系统

--- lao_stu_manage.c
+++ lao_stu_manage.c
@@ -1,3 +1,10 @@
+/*此函数是学生管理系统，主函数有两个，一个时常规函数，一个用到来getchar(),
+*
+
+
+
+
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -18,22 +25,22 @@ stu *add_link(stu *head)
         perror("create");
         exit(0);
     }
-    printf("please input tht number :");
+    printf("please input tht number :\n");
     scanf("%d",&p_c->number);
-    printf("please input the name :");
+    printf("please input the name :\n");
     scanf("%s",p_c->name);
+    getchar();
     p_c->next=NULL;
     if (NULL==ptr)
     {
         return p_c;
     }
-    if (p_c->number<head->number)
+    if (p_c->number < head->number)
     {
         p_c->next=head;
         return p_c;
     }
-    printf("0");
-    while((ptr->next!=NULL)&&(p_c->number>ptr->next->number))
+    while((ptr->next!=NULL)&&(p_c->number > ptr->next->number))
     {
         ptr=ptr->next;
     }
@@ -44,16 +51,24 @@ stu *add_link(stu *head)
 stu *delete_link(stu *p)
 {
     stu *h=p;
-    stu *temp;
+    stu *temp=NULL;
     int i=0;
+    printf("please input the delete number :\n");
     scanf("%d",&i);
+    getchar();
+    if(p==NULL)
+    {
+        printf("empty\n");
+        return p;
+    }
     if (p->number==i)
     {
-        h=p->next;
-        free(p);
+        temp=p;/////////
+        h=p=p->next;
+        free(temp);
         return h;
     }
-    while(p!=NULL)
+    while(p->next!=NULL)
     {
         if (p->next->number==i)
         {
@@ -73,11 +88,17 @@ stu *delete_link(stu *p)
 }
 void printf_link(stu *p)
 {
+    if(p==NULL)
+    {
+        printf("empty\n");
+        return;
+    }
     while(p!=NULL)
     {
-        printf("%d     %s",p->number,p->name);    
+        printf("%d     %s\n",p->number,p->name);    
         p=p->next;
     }
+    return ;
 }
 char menu(void)
 {
@@ -88,30 +109,69 @@ char menu(void)
     puts("4. 存储");
     puts("5. 退出");
 }
+void save_link(stu *p)
+{
+    FILE *fp=NULL;
+    fp=fopen("1.txt","w+");
+    if (fp==NULL) 
+    {
+        perror("open");
+        exit(0);
+    }
+    while(p!=NULL)
+    {
+        fprintf(fp,"%d    %s\n",p->number,p->name);
+        p=p->next;
+    }
+    fclose(fp);
+}
+/*
 int main(int argc, const char *argv[])
 {
     stu *head = NULL;
     char flag=1;
+    int  choice=0;
+    while(flag>0)
+    {
+        menu();
+       // choice=getchar();
+        scanf("%d", &choice);
+       // getchar();
+        
+        switch(choice)
+        {   
+            case 1:head = add_link(head);break;
+            case 2:head = delete_link(head);break;
+            case 3:printf_link(head);break;
+            case 4:save_link(head);break;
+            case 5:flag=0;break;
+            default : printf("num is no\n"); break;
+        }
+    }
+    printf("\n");
+    return 0;
+}*/
+int main(int argc, const char *argv[])/////用此主函数时，应该注意在输入函数中添加getchar(),用来消除缓冲区的回车键。。此函数中在插入 和 删除函数中都要用到。
+{
+    stu *head=NULL;
+    char flag=1;
     char choice=0;
+    
     while(flag>0)
     {
         menu();
         choice=getchar();
         getchar();
-        printf("%d\n",choice-48);
-        //choice=choice-48;
-        switch(choice-48)
-        {   
-            case 1:add_link(head);break;
-            case 2:delete_link(head);break;
-            case 3:printf_link(head);break;
-            case 4:break;
-            case 5:flag=0;break;
+        switch(choice)
+        {
+            case '1':head=add_link(head);break;
+            case '2':head=delete_link(head);break;
+            case '3':printf_link(head);break;
+            case '4':save_link(head);break;
+            case '5':flag=0;break;
+            default : printf("no number\n");break;
         }
     }
-    //stu *h;
-    //h =  add_link(head);
-
-    printf_link(head);
+    printf("\n");
     return 0;
 }
