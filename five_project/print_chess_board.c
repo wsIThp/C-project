ab339c2910b260a0472335ac9de221a68fe48ab7
xiaoxin:wq <593748946@qq.com>
2011-08-17 14:53:16 +0800

    0817

---
+++ five_project/print_chess_board.c
@@ -0,0 +1,17 @@
+/*这是划出五子棋棋盘的函数*/
+
+#include <stdio.h>
+#include "various.h"
+
+void printf_board(void)
+{
+	int i=0;
+	for (i = 0; i < Y_NUM; i++)
+	{
+		fb_line(ST_X,ST_Y+i*SPACE,ST_X+(X_NUM-1)*SPACE,ST_Y+i*SPACE,0x00ffffff);
+	}
+	for (i = 0; i < X_NUM; i++) 
+	{
+		fb_line(ST_X+i*SPACE,ST_Y,ST_X+i*SPACE,ST_Y+(Y_NUM-1)*SPACE,0X00ffffff);
+	}
+}
